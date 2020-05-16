196c33ad4d0778b3f2d1d3da13c63b58927dd70a
Lee JE <jes7077@naver.com>
2020-05-16 21:36:40 +0900

    Build DataSource connection pool w/ HikariCP

--- SpringTest/src/main/java/org/zerock/config/RootConfig.java
+++ SpringTest/src/main/java/org/zerock/config/RootConfig.java
@@ -1,10 +1,26 @@
 package org.zerock.config;
 
+import javax.sql.DataSource;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import com.zaxxer.hikari.HikariConfig;
+import com.zaxxer.hikari.HikariDataSource;
 
 @Configuration
 @ComponentScan(basePackages = {"org.zerock.sample"})
 public class RootConfig {
 	
+	@Bean
+	public DataSource dataSource() {
+		HikariConfig hikariConfig = new HikariConfig();
+		hikariConfig.setDriverClassName("oracle.jdbc.driver.OracleDriver");
+		hikariConfig.setJdbcUrl("jdbc:oracle:thin:@localhost:1521:xe");
+		hikariConfig.setUsername("System");
+		hikariConfig.setPassword("admin1234");
+		
+		HikariDataSource dataSource = new HikariDataSource(hikariConfig);
+		
+		return dataSource;
+	}
 }
