7fe7f4b1ebc9787b75129ff928bfbb872d49d2f4
Lee JE <jes7077@naver.com>
2020-05-19 21:33:36 +0900

    boardController(list, register) test

---
+++ SpringTest/src/main/java/org/zerock/controller/BoardController.java
@@ -0,0 +1,37 @@
+package org.zerock.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+import org.zerock.domain.BoardVO;
+import org.zerock.service.BoardService;
+
+import lombok.AllArgsConstructor;
+import lombok.extern.log4j.Log4j;
+
+@Controller
+@Log4j
+@RequestMapping("/board/*")
+@AllArgsConstructor //아니면 @Setter(onMethod_) 처리해서 생성자 만들어 주입 
+public class BoardController {
+	private BoardService service;
+	
+	@GetMapping("/list")
+	public void list(Model model) {
+		log.info("/list called");
+		model.addAttribute("list", service.getList());
+	}
+	
+	@PostMapping("/register")
+	public String register(BoardVO board, RedirectAttributes rttr) {
+		log.info("/register called" + board);
+		service.register(board);
+		rttr.addFlashAttribute("result", board.getBno());
+		
+		return "redirect:/board/list"; 
+		//등록 작업이 끝난 후 다시 목록화면으로 이동, response.sendRedirect()와 같은 개념
+	}
+}
--- SpringTest/src/test/java/org/zerock/service/BoardServiceTest.java
+++ SpringTest/src/test/java/org/zerock/service/BoardServiceTest.java
@@ -15,7 +15,7 @@ import lombok.extern.log4j.Log4j;
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration(classes = {org.zerock.config.RootConfig.class})
 @Log4j
-public class BoardServieTest {
+public class BoardServiceTest {
 	
 	@Setter(onMethod_ = {@Autowired})
 	private BoardService service;
