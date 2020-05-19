e0d848318b0cca0e00d24d74290952a1edace244
Lee JE <jes7077@naver.com>
2020-05-19 19:08:35 +0900

    create(insert) 처리

--- SpringTest/src/main/java/org/zerock/mapper/BoardMapper.java
+++ SpringTest/src/main/java/org/zerock/mapper/BoardMapper.java
@@ -10,4 +10,6 @@ public interface BoardMapper {
 	//@Select("select * from tbl_board where bno > 0") //주의: 세미콜론 없이 sql문을 써야함
 	//xml페이지에서 sql문 처리 했다면 해당 annnotation을 쓸 필요가 없음
 	public List<BoardVO> getList();
+	public void insert(BoardVO board);
+	public void insertSelectKey(BoardVO board);
 }
--- SpringTest/src/main/java/org/zerock/mapper/BoardMapperTest.java
+++ SpringTest/src/main/java/org/zerock/mapper/BoardMapperTest.java
@@ -5,6 +5,7 @@ import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.zerock.domain.BoardVO;
 
 import lombok.Setter;
 import lombok.extern.log4j.Log4j;
@@ -20,5 +21,29 @@ public class BoardMapperTest {
 	@Test
 	public void testGetList() {
 		mapper.getList().forEach(board -> log.info(board));
+	}
+	
+	@Test
+	public void testInsert() {
+		BoardVO board = new BoardVO();
+		board.setTitle("새로작성");
+		board.setContent("새로작성내용");
+		board.setWriter("testuser2");
+		
+		mapper.insert(board);
+		
+		log.info(board);
+	}
+	
+	@Test //@SelectKey를 이용하는 테스트 코드
+	public void testInsertSelectKey() {
+		BoardVO board = new BoardVO();
+		board.setTitle("select key 새로운 글");
+		board.setContent("select key 새로운 내용");
+		board.setWriter("testuser3");
+		
+		mapper.insertSelectKey(board);
+		
+		log.info(board);
 	}
 }
