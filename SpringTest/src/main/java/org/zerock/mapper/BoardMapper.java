c45d9ddaf62f807568345cece965db73b8cfb791
Lee JE <jes7077@naver.com>
2020-05-19 18:28:42 +0900

    sql xml 처리

--- SpringTest/src/main/java/org/zerock/mapper/BoardMapper.java
+++ SpringTest/src/main/java/org/zerock/mapper/BoardMapper.java
@@ -7,6 +7,7 @@ import org.zerock.domain.BoardVO;
 
 public interface BoardMapper {
 	
-	@Select("select * from tbl_board where bno > 0") //주의: 세미콜론 없이 sql문을 써야함 
+	//@Select("select * from tbl_board where bno > 0") //주의: 세미콜론 없이 sql문을 써야함
+	//xml페이지에서 sql문 처리 했다면 해당 annnotation을 쓸 필요가 없음
 	public List<BoardVO> getList();
 }
