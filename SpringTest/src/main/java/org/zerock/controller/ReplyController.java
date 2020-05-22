66aef0bdb81d676afa756ad1e14d8472528bc8ab
Lee JE <jes7077@naver.com>
2020-05-22 21:41:34 +0900

    Reply CRUD Rest test

--- SpringTest/src/main/java/org/zerock/controller/ReplyController.java
+++ SpringTest/src/main/java/org/zerock/controller/ReplyController.java
@@ -1,13 +1,20 @@
 package org.zerock.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+import org.zerock.domain.Criteria;
 import org.zerock.domain.ReplyVO;
 import org.zerock.service.ReplyService;
 
@@ -34,5 +41,34 @@ public class ReplyController {
 		
 		return insertCount == 1 ? new ResponseEntity<>("success", HttpStatus.OK) : new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
 		//결과가 제대로 전달이 되었는지 확인해줌(삼항연산자로 처리)
+	}
+	
+	@GetMapping(value = "/pages/{bno}/{page}", produces = {MediaType.APPLICATION_XML_VALUE, MediaType.APPLICATION_JSON_UTF8_VALUE})
+	public ResponseEntity<List<ReplyVO>> getList(@PathVariable("page") int page, @PathVariable("bno") Long bno) {
+		log.info("/getList called");
+		Criteria cri = new Criteria(page, 10);
+		log.info(cri);
+		return new ResponseEntity<>(service.getList(cri, bno), HttpStatus.OK);
+	}
+	
+	@GetMapping(value = "/{rno}", produces = {MediaType.APPLICATION_XML_VALUE, MediaType.APPLICATION_JSON_UTF8_VALUE})
+	public ResponseEntity<ReplyVO> get(@PathVariable("rno") Long rno) {
+		log.info("/get called: " + rno);
+		return new ResponseEntity<>(service.get(rno), HttpStatus.OK);
+	}
+	
+	@DeleteMapping(value = "/{rno}", produces = {MediaType.TEXT_PLAIN_VALUE})
+	public ResponseEntity<String> remove(@PathVariable("rno") Long rno) {
+		log.info("/remove called: " + rno);
+		
+		return service.remove(rno) == 1 ? new ResponseEntity<>("success", HttpStatus.OK) : new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+	}
+	
+	@RequestMapping(method= {RequestMethod.PUT, RequestMethod.PATCH}, value="/{rno}", consumes="application/json", produces = {MediaType.TEXT_PLAIN_VALUE})
+	public ResponseEntity<String> modify(@RequestBody ReplyVO vo, @PathVariable("rno") Long rno) {
+		vo.setRno(rno);
+		log.info("rno: " + rno);
+		log.info("/modify called: "+vo);
+		return service.modify(vo) == 1 ? new ResponseEntity<>("success", HttpStatus.OK) : new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
 	}
 }
--- SpringTest/src/test/java/org/zerock/mapper/ReplyMapperTest.java
+++ SpringTest/src/test/java/org/zerock/mapper/ReplyMapperTest.java
@@ -1,5 +1,6 @@
 package org.zerock.mapper;
 
+import java.util.List;
 import java.util.stream.IntStream;
 
 import org.junit.Test;
@@ -7,6 +8,7 @@ import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.zerock.domain.Criteria;
 import org.zerock.domain.ReplyVO;
 
 import lombok.Setter;
@@ -23,19 +25,19 @@ public class ReplyMapperTest {
 	@Setter(onMethod_ = @Autowired)
 	private ReplyMapper mapper;
 	
+	/*
+	 * @Test public void testCreate() { IntStream.rangeClosed(1, 10).forEach(i ->{
+	 * ReplyVO vo = new ReplyVO();
+	 * 
+	 * //게시물 번호 vo.setBno(bnoArr[i%2]); vo.setReply("댓글테스트" + i);
+	 * vo.setReplyer("replyer" + i);
+	 * 
+	 * mapper.insert(vo); }); }
+	 */
 	@Test
-	public void testCreate() {
-		IntStream.rangeClosed(1, 10).forEach(i ->{
-			ReplyVO vo = new ReplyVO();
-			
-			//게시물 번호
-			vo.setBno(bnoArr[i%2]);
-			vo.setReply("댓글테스트" + i);
-			vo.setReplyer("replyer" + i);
-			
-			mapper.insert(vo);
-		});
+	public void testList() {
+		Criteria cri = new Criteria();
+		List<ReplyVO> replies = mapper.getListWithPaging(cri, bnoArr[0]);
+		replies.forEach(reply -> log.info(reply));
 	}
-	
-	
 }
